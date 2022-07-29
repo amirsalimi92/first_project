0671ae2d2531cea394b415864e0da94079c34003
amirsalimi92 <amir.salimi1810@gmail.com>
2022-07-29 15:11:19 +0200

    first

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// my libraries
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
