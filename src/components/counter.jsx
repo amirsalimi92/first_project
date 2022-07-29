b06b71144193b86efafea43e06b36361746a93e8
amirsalimi92 <amir.salimi1810@gmail.com>
2022-07-29 22:31:29 +0200

    29_july

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  style = {
+    fontSize: 30,
+    fontWeight: "bold",
+  };
+
   render() {
+    let classes = this.getButtonClasses();
     return (
-      <React.Fragment>
-        <h1>Hello World!</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={this.style} className="m-3">
+          {this.formatCount()}
+        </span>
+        <br />
+        <button className={classes}>Increment</button>
+      </div>
     );
+  }
+
+  getButtonClasses() {
+    let classes = "m-2 btn ";
+    classes += this.state.count === 0 ? "btn-warning" : "btn-primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
   }
 }
 
