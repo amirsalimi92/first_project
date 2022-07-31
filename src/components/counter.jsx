1a0d267be8912c4dcffe0b568e2f4de28384abfa
amirsalimi92 <amir.salimi1810@gmail.com>
2022-07-31 02:30:54 +0200

    31_july_first

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,33 +5,38 @@ class Counter extends Component {
     count: 0,
   };
 
-  style = {
-    fontSize: 30,
-    fontWeight: "bold",
-  };
-
-  render() {
-    let classes = this.getButtonClasses();
+  renderTag() {
+    if (this.state.tags.length === 0) return <p>There is no tag!</p>;
     return (
-      <div>
-        <span style={this.style} className="m-3">
-          {this.formatCount()}
-        </span>
-        <br />
-        <button className={classes}>Increment</button>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getButtonClasses() {
-    let classes = "m-2 btn ";
-    classes += this.state.count === 0 ? "btn-warning" : "btn-primary";
-    return classes;
+  buttonClass() {
+    let base = "m-3 btn ";
+    if (this.state.count === 0) return (base += "btn-warning");
+    if (this.state.count % 2 === 1) return (base += "btn-success");
+    return (base += "btn-primary");
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.increment} className={this.buttonClass()}>
+          Increment
+        </button>
+      </div>
+    );
   }
 }
 
